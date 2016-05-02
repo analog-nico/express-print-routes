1e6ce7414233d95dab75c71f7a9ee6311877a43a
analog-nico <nicolai.kamenzky@testrails.org>
2016-05-01 21:52:15 -0700

    chore: improved test coverage

--- test/spec/main.js
+++ test/spec/main.js
@@ -16,18 +16,33 @@ describe('The express-print-routes middleware', function () {
         app.all('*', function __starAll() {});
         app.get('/', function __rootGet() {});
         app.get('/test', function __testGet() {});
-        app.post('/test/:param', function __testParamPost() {});
+        //app.param('testId', function __testIdParam() {});
+        app.post('/test/:testId', function __testParamPost() {});
         app.get(/^\/spa($|\/)/, function __spaRegexGet() {});
         app.get('/chained', function __chainedGet1() {}, function __chainedGet2() {});
+        app.use('/use', function __useUse() {});
 
         var router = express.Router();
-        app.use('/routed', router);
+        app.use('/routedWithRouter', router);
         router.all('*', function __routedStarAll() {});
         router.get('/', function __routedRootGet() {});
         router.get('/test', function __routedTestGet() {});
-        router.post('/test/:param', function __routedTestParamPost() {});
+        //router.param('testId', function __routedTestIdParam() {});
+        router.post('/test/:testId', function __routedTestParamPost() {});
         router.get(/^\/spa($|\/)/, function __routedSpaRegexGet() {});
         router.get('/chained', function __routedChainedGet1() {}, function __routedChainedGet2() {});
+        router.use('/use', function __routedUseUse() {});
+        router.route('/routedWithDotRoute')
+            .all(function __routedAll() {})
+            .get(function __routedChainedGet1() {}, function __routedChainedGet2() {});
+
+        app.route('/routedWithDotRoute')
+            .all(function __routedAll() {})
+            .get(function __routedChainedGet1() {}, function __routedChainedGet2() {});
+
+        app.route(/^\/routedWithDotRouteAndRegex($|\/)/)
+            .get(function __routedGet() {})
+            .post(function __routedChainedPost1() {}, function __routedChainedPost2() {});
 
 
         printRoutes(app, path.join(__dirname, '../results/routes.generated.txt'));
@@ -39,7 +54,7 @@ describe('The express-print-routes middleware', function () {
                 return process.versions.node.split('.')[0];
             }
 
-            var expected = fs.readFileSync(path.join(__dirname, '../results/routes.expected.node' + getNodeVersionMajor() + '.txt'), 'utf8');
+            var expected = fs.readFileSync(path.join(__dirname, '../fixtures/routes.expected.node' + getNodeVersionMajor() + '.txt'), 'utf8');
             var generated = fs.readFileSync(path.join(__dirname, '../results/routes.generated.txt'), 'utf8');
 
             expect(generated).to.eql(expected);
@@ -62,7 +77,7 @@ describe('The express-print-routes middleware', function () {
         router.get('/users/:id', function __getUser() {});
         router.post('/users/:id', function __checkAccessRights() {}, function __updateUser() {});
 
-        app.use(function serveStatic() {});
+        app.use(express.static('.'));
         app.use(function __handleError() {});
 
         printRoutes(app, path.join(__dirname, '../results/example.generated.txt'));
@@ -70,7 +85,7 @@ describe('The express-print-routes middleware', function () {
 
         setTimeout(function () {
 
-            var expected = fs.readFileSync(path.join(__dirname, '../results/example.expected.txt'), 'utf8');
+            var expected = fs.readFileSync(path.join(__dirname, '../fixtures/example.expected.txt'), 'utf8');
             var generated = fs.readFileSync(path.join(__dirname, '../results/example.generated.txt'), 'utf8');
 
             expect(generated).to.eql(expected);
