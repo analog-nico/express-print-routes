405cd0ed6a0b0a85114b074797c26789799d91f9
analog-nico <nicolai.kamenzky@testrails.org>
2016-05-01 17:51:59 -0700

    feat: improved test coverage

--- test/spec/main.js
+++ test/spec/main.js
@@ -13,9 +13,25 @@ describe('The express-print-routes middleware', function () {
 
         var app = express();
 
-        app.get('/test', function __getTest() {});
+        app.all('*', function __starAll() {});
+        app.get('/', function __rootGet() {});
+        app.get('/test', function __testGet() {});
+        app.post('/test/:param', function __testParamPost() {});
+        app.get(/^\/spa($|\/)/, function __spaRegexGet() {});
+        app.get('/chained', function __chainedGet1() {}, function __chainedGet2() {});
+
+        var router = express.Router();
+        app.use('/routed', router);
+        router.all('*', function __routedStarAll() {});
+        router.get('/', function __routedRootGet() {});
+        router.get('/test', function __routedTestGet() {});
+        router.post('/test/:param', function __routedTestParamPost() {});
+        router.get(/^\/spa($|\/)/, function __routedSpaRegexGet() {});
+        router.get('/chained', function __routedChainedGet1() {}, function __routedChainedGet2() {});
+
 
         printRoutes(app, path.join(__dirname, '../results/routes.generated.txt'));
+
 
         setTimeout(function () {
 
